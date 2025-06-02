a2511ca9ae229c4668c3bcdcf68942205012c306
Patrick Breslin <patrickbfx15@gmail.com>
2025-06-02 16:04:35 -0400

    Fix bug with unit label display
    Meter no longer moves to the left when unit is damaged

--- unit_label.gd
+++ unit_label.gd
@@ -19,6 +19,15 @@ func initialize(name, crntHealth, maxHealth):
 	$UnitName.text = unitName
 	$HealthNumber.text = "     HP:   "+str(HP)+"/"+str(maxHP)
 	$HPGaugeMeter.apply_scale(Vector2(float(HP)/float(maxHP),1.0))
+	#$HPGaugeMeter.position = $HPGaugeBackground.position
+	print("Meter position:      "+str($HPGaugeMeter.position))
+	print("Background position: "+str($HPGaugeBackground.position)+"\n")
+	#print($HPGaugeBackground.polygon)
+	#queue_redraw()
 	
+#func _draw():
+	#draw_rect(Rect2(12.0,48.0,73.0,16.0),Color.WHITE)
+	#draw_rect(Rect2(12.0,48.0,float(73.0*HP/maxHP),16.0),Color.WHITE)
+
 func delete():
 	queue_free()
