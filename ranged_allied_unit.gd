de2195252530d7a77a48a4aa7abc796048646f46
Patrick Breslin <patrickbfx15@gmail.com>
2025-07-24 14:10:01 -0400

    Add allied ranged unit
    Note: Does not add implementation for ranged attacks

---
+++ ranged_allied_unit.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+
+var selected = false
+var isReady = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var MAXHP = 6
+	var SPEED = 4
+	var ATTACK = 7
+	var DEFENSE = 0
+	var NAME = "Good Guy"
+	$Unit.initialize(NAME,MAXHP,SPEED,ATTACK,DEFENSE)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not(self.has_node("Unit")):
+		queue_free()
+	if not(self.has_node("Unit")):
+		get_parent().removeAlly(self)
+		queue_free()
+
+func getSpeed():
+	return $Unit.speed
+
+func getAttack():
+	return $Unit.attack
+
+func getHP():
+	return $Unit.hitPoints
+
+func getMaxHP():
+	return $Unit.maxHitPoints
+
+func getName():
+	return $Unit.unitName
+
+func getDefense():
+	return $Unit.defense
+
+func resetDefense():
+	$Unit.resetDefense()
+
+func takeHit(dmg:int, isBlockable:bool):
+	$Unit.takeHit(dmg,isBlockable)
+
+func toggleSelect(sel:bool):
+	selected = sel
+	if selected:
+		$AnimatedSprite2D.animation = "selected"
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.animation = "base"
+		$AnimatedSprite2D.stop()
+
+func toggleReady(ready):
+	isReady = ready
+
+func _to_string():
+	return "HP="+str(getHP())+", Def="+str(getDefense())
